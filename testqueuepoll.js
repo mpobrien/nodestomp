e7cff8868654ef0c122eb677bfb813edfff9ad0d
Mike O'Brien <mikeo@10gen.com>
2012-04-11 08:48:15 -0400

    test custom op

--- testqueuepoll.js
+++ testqueuepoll.js
@@ -2,9 +2,12 @@ var PollQueue = require('./queuepoll').PollQueue;
 
 var pq = new PollQueue('localhost', 27017, "queue", "queue", 
     {maxInterval : 500,
-     remove      : true});
+        op: {"$set":{"consumed":true}}
+    });
+     //remove      : true});
 
-pq.addQuery("blah", {});
+//pq.addQuery("blah", {});
+pq.addQuery("asdf", {consumed:false});
 
 pq.on("message", function(doc, filterId){
     console.log("received from ", filterId, doc);
